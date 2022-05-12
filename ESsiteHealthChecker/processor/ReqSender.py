1429f70860e6d8e52d7492b794ce42a4641c2965
Fengchl <chunlai.feng@redteamobile.com>
2022-05-12 12:24:34 +0800

    增加界面模块

--- ESsiteHealthChecker/processor/ReqSender.py
+++ ESsiteHealthChecker/processor/ReqSender.py
@@ -1,3 +1,12 @@
-import httplib
-conn = httplib.HTTPConnection("www.python.org")
+import httplib2
+conn = httplib2.HTTPConnectionWithTimeout("www.baidu.com")
+conn.request("GET", "/index.html")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 
+data1 = r1.read()
+conn.request("GET", "/parrot.spam")
+r2 = conn.getresponse()
+print(r2.status, r2.reason)
+data2 = r2.read()
+conn.close()
